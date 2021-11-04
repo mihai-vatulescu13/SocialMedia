a6afaaeba75b081a99ea1e8abe224a019c58962b
Mihai Vatulescu <mihai.vatulescu@nagarro.com>
2021-11-04 18:12:52 +0200

    first deploy

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express')
-const authRouters= require('./routes/auth')
+const authRouters = require('./routes/auth')
 
 const app = express();
 const port = process.env.PORT || 5000
 
 app.use('/api/auth', authRouters)
 
-if(process.env.NODE_ENV==='production'){
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
-    const path= require('path')
-    app.get('*', (req, res)=>{
+    const path = require('path')
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
-app.listen(port, ()=>console.log(`Sv is running on port: ${port}`))
+app.listen(port, () => console.log(`Sv is running on port: ${port}`))
