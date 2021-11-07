ea53fe69ee203c47f4ef81c41760118fc936ce98
Mihai Vatulescu <mihai.vatulescu@nagarro.com>
2021-11-07 18:27:34 +0200

    removed some useless files

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,8 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Register from "./pages/Register/Register";
-import Login from "./pages/Login/Login";
-import EditAccount from "./pages/EditAccount/EditAccount"
-import NavBar from "./components/navbar/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to social you!</h1>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={Home} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="editAccount" element={<EditAccount />} />
-      </Routes>
+      <h1>Welcome to React Redux!</h1>
     </div>
   );
 }
