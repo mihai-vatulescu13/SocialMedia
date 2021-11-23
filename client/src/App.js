e5e59fc25e440021e0f0aa9e1719cd845960e1f1
Daniel <truicadaniel72@gmail.com>
2021-11-23 11:12:07 +0200

    home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,33 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Register from "./pages/Register/Register";
-import Login from "./pages/Login/Login";
-import EditAccount from "./pages/EditAccount/EditAccount"
-import { connect } from "react-redux";
-import "./App.css";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Register from './pages/Register/Register';
+import Login from './pages/Login/Login';
+import EditAccount from './pages/EditAccount/EditAccount';
+import { connect } from 'react-redux';
+import './App.css';
 
 function App({ connectedUser }) {
-
   return (
     <div className="App">
-      <h1>Welcome to social you!</h1>
       <Routes>
-        <Route path="/" element={connectedUser.name? <Home /> : <Navigate replace to="/login" />} />
-        <Route path="/login" element={connectedUser.name ? <Navigate replace to="/"/> : <Login/>} />
-        <Route path="/register" element={connectedUser.name ? <Navigate replace to="/"/> : <Register />} />
+        <Route
+          path="/"
+          element={
+            connectedUser.name ? <Home /> : <Navigate replace to="/login" />
+          }
+        />
+        <Route
+          path="/login"
+          element={connectedUser.name ? <Navigate replace to="/" /> : <Login />}
+        />
+        <Route
+          path="/register"
+          element={
+            connectedUser.name ? <Navigate replace to="/" /> : <Register />
+          }
+        />
         <Route path="/editAccount" element={<EditAccount />} />
-        <Route path="/myAccount/:user"/>
+        <Route path="/myAccount/:user" />
       </Routes>
     </div>
   );
@@ -24,8 +35,8 @@ function App({ connectedUser }) {
 
 const mapStateToProps = (state) => {
   return {
-    connectedUser: state.AuthReducer
-  }
-}
+    connectedUser: state.AuthReducer,
+  };
+};
 
 export default connect(mapStateToProps)(App);
---
+++ client/src/components/testData/homeCard.js
@@ -0,0 +1,12 @@
+const PF = process.env.REACT_APP_ASSETS;
+console.log(PF);
+export const objCards = [
+  {
+    name: 'Andrei',
+    photo: PF + '1.jpg',
+  },
+  {
+    name: 'Marcel',
+    photo: PF + '2.jpg',
+  },
+];
