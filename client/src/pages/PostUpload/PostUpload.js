46dbd85bf94c2660abd04ee76bea348a72860b09
mihai vatulescu <mihai.vatulescu@nagarro.com>
2021-11-27 15:14:25 +0200

    uploaded img as base 64

--- client/src/pages/PostUpload/PostUpload.js
+++ client/src/pages/PostUpload/PostUpload.js
@@ -1,19 +1,38 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "./postUpload.css";
 
 const PostUpload = () => {
+  const [image, setImage] = useState(null);
+
   const description = useRef();
   const location = useRef();
   const postImage = useRef();
 
   const onUpload = (e) => {
     e.preventDefault();
-    console.log(
-      "data:",
-      description.current.value,
-      location.current.value,
-      postImage.current.files[0]
-    );
+    let file = postImage.current.files[0];
+
+    const uploadImage = async () => {
+      const base64 = await converBase64(file);
+      console.log("converted image:", typeof base64);
+
+      setImage(base64);
+    };
+
+    const converBase64 = (file) => {
+      return new Promise((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+        fileReader.onload = () => {
+          resolve(fileReader.result);
+        };
+        fileReader.onerror = (error) => {
+          reject(error);
+        };
+      });
+    };
+
+    uploadImage();
   };
 
   return (
@@ -46,6 +65,18 @@ const PostUpload = () => {
             send
           </button>
         </form>
+
+        <div className="preview-image">
+          {image ? (
+            <img
+              src={image}
+              style={{ width: "100px", height: "auto" }}
+              alt="preview"
+            />
+          ) : (
+            <></>
+          )}
+        </div>
       </div>
     </div>
   );
