ca1ea707848c4bac0f4b584e94100bdd970404e1
Daniel <truicadaniel72@gmail.com>
2021-12-15 15:48:22 +0200

    backup post

--- client/src/components/PostModal/PostModal.js
+++ client/src/components/PostModal/PostModal.js
@@ -1,20 +1,26 @@
-import "./postModal.css";
+import './postModal.css';
 
 const PostModal = ({ post, setOpenedPostModal }) => {
   return (
     <div className="background-modal-container">
-      <div className="post-modal-container">
-        <div className="picture-post-container">
-          <img src={post.image} alt="post" className="post-img" />
-        </div>
-        <div className="description-actions-container">
-          <h3>{post.description}</h3>
-          <button
-            className="close-post-modal-btn"
-            onClick={() => setOpenedPostModal()}
-          >
-            Close
-          </button>
+      <div className="card-modal-container">
+        <img src={post.image} alt="post" className="post-img" />
+        <div className="info-post">
+          <div className="user-post">
+            <img src={post.image} alt="userImage" className="userImage" />
+            <span className="post-manu"> &#8230;</span>
+          </div>
+          <div className="comments-section">
+            <h3>{post.description}</h3>
+            <button
+              className="close-post-modal-btn"
+              onClick={() => setOpenedPostModal()}
+            >
+              Close
+            </button>
+          </div>
+          <div className="react-socialbar">ba</div>
+          <div className="input-sendComment">ba</div>
         </div>
       </div>
     </div>
