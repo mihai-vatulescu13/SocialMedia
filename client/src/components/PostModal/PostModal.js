49dc8b86d7b4e5b89ec7531ea4b02fa21dc15899
mihai vatulescu <mihai.vatulescu@nagarro.com>
2021-12-16 17:57:27 +0200

    changed date time posted approach

--- client/src/components/PostModal/PostModal.js
+++ client/src/components/PostModal/PostModal.js
@@ -1,11 +1,10 @@
 import { useEffect } from "react";
-import { useState } from "react";
 import { useRef } from "react";
+import { format } from "timeago.js";
 import "./postModal.css";
 
 const PostModal = ({ post, setOpenedPostModal, userData }) => {
   const cardRef = useRef();
-  const [currentDate, setCurrentDate] = useState(new Date().getTime());
 
   useEffect(() => {
     const outsideClick = (e) => {
@@ -18,6 +17,7 @@ const PostModal = ({ post, setOpenedPostModal, userData }) => {
       document.removeEventListener("click", outsideClick);
     };
   }, []);
+
   return (
     <div className="background-modal-container">
       <div className="card-modal-container" ref={cardRef}>
@@ -35,14 +35,7 @@ const PostModal = ({ post, setOpenedPostModal, userData }) => {
             <span className="post-menu"> &#8230;</span>
           </div>
           <div className="comments-section">
-            <h5 className="posted-date">
-              {parseInt(
-                (currentDate -
-                  new Date(`${post.createdAt.slice(0, 10)}`).getTime()) /
-                  (1000 * 3600 * 24)
-              )}{" "}
-              days ago
-            </h5>
+            <h5 className="posted-date">{format(post.createdAt)}</h5>
             <h3 className="post-desc-heading">{post.description}</h3>
           </div>
           <div className="react-socialbar">ba</div>
