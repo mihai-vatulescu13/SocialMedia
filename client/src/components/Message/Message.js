cc4594c5112df88e6915e9f49aab2e056d11beb5
mihai vatulescu <mihai.vatulescu@nagarro.com>
2022-02-18 13:31:14 +0200

    added alt for user message avatar

--- client/src/components/Message/Message.js
+++ client/src/components/Message/Message.js
@@ -4,7 +4,7 @@ export default Message = ({ userPicture, messageContent }) => {
   return (
     <div className="message-container">
       <div className="message-user-picture">
-        <img src="" alt="" />
+        <img src={userPicture} alt="user message avatar" />
       </div>
       <div className="message-user-content">
         <p className="message-content-style">message content</p>
